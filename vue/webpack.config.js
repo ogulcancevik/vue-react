1e37e02c6d7828bdcc3c0e01aeb4dc2cd4cee83c
Oğulcan ÇEVİK <cancevdev@gmail.com>
2022-02-09 11:24:19 +0300

    argv dev added.

--- vue/webpack.config.js
+++ vue/webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:8080/"
+        : "https://vue-react.vercel.app/",
   },
 
   resolve: {
@@ -49,4 +52,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
