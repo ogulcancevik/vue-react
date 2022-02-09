4448ead66f6892315c8adeeb80a415e80dfaa3f2
Oğulcan ÇEVİK <cancevdev@gmail.com>
2022-02-09 13:08:01 +0300

    init

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: argv.mode === 'development' ? "http://localhost:8081/" : 'https://react-bice-three.vercel.app/',
+    publicPath: argv.mode === 'development' ? "http://localhost:8081/" : 'https://vue-react.vercel.app/',
   },
 
   resolve: {
